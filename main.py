4b1195ce5798ecf69fcf7bedf05f618bebd63051
lifeisdarkin1984 <lifeisdarkin1984@gmail.com>
2026-06-25 01:25:02 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ API_HASH  = os.environ["API_HASH"]
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 from handlers import login, text_handler, callbacks
-from workers import secretary, scheduler, reply_worker, react_worker, global_scheduler
+from workers import secretary, scheduler, reply_worker, react_worker, global_scheduler, pv_monitor
 
 # توقف عملیات - global flag
 stop_flags = {}
@@ -33,6 +33,7 @@ async def main():
     print("✅ Tabchi Personal bot is running...")
     global_scheduler.BOT_CLIENT = app
     secretary.BOT_CLIENT = app
+    pv_monitor.BOT_CLIENT = app
 
     await asyncio.gather(
         secretary.run(),
@@ -40,6 +41,7 @@ async def main():
         reply_worker.run(),
         react_worker.run(),
         global_scheduler.run(),
+        pv_monitor.run(),
         idle(),
     )
 
